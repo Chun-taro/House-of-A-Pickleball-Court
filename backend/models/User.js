ae0de916a66700c2a42c32fd8fd88a4607a2782a
Chun-taro <chun-taro@users.noreply.github.com>
2026-07-26 21:39:34 +0800

    Fix User schema pre-save hook for async execution

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,11 +15,12 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  // If password is already a bcrypt hash (starts with $2a$ or $2b$), skip re-hashing
+  if (this.password.startsWith('$2a$') || this.password.startsWith('$2b$')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
